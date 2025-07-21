afb3f3d8625c64b8f3912703e824af9ac34658cb
bfranks56 <bryant.franks@gmail.com>
2025-07-21 15:30:02 -0400

    update CRUD calls

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+app.get('/users', (req, res) => {
+    return res.send('Received a GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+    return res.send('Received a POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+    return res.send(`Received a PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete('/users/:userId', (req, res) => {
+    return res.send(`Received a DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 app.listen(process.env.PORT, () => {
