c3feaba0d61910e0a81920313edb5f1f607d4cb3
bfranks56 <bryant.franks@gmail.com>
2025-07-21 16:47:45 -0400

    update get users and get messages

--- index.js
+++ index.js
@@ -10,8 +10,44 @@ app.use(cors());
 //     res.send('Hello, World!');
 // });
 
+let users = {
+    1: {
+        id: '1',
+        userName: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        userName: 'Dave Davids',
+    }
+};
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello, World!',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Hello, Dave!',
+        userId: '2',
+    }
+};
+
 app.get('/users', (req, res) => {
-    return res.send('Received a GET HTTP method on user resource');
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId])
 });
 
 app.post('/users', (req, res) => {
